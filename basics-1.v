add20e7466ffafcbf68c8147e237736b18e164ff
Luiz Damasceno <luizedc1@gmail.com>
2023-11-24 00:12:37 -0300

    unary numeric base

--- basics-1.v
+++ basics-1.v
@@ -148,3 +148,60 @@ Definition foo : bool := true.
 
 Check Playground.foo : rgb.
 Check foo : bool.
+
+Module TuplePlayground.
+
+Inductive bit : Type :=
+  | B1
+  | B0.
+
+Inductive nybble : Type :=
+  | bits (b0 b1 b2 b3 : bit).
+
+Check (bits B1 B0 B1 B0)
+  : nybble.
+
+Definition all_zero (nb : nybble) : bool :=
+  match nb with
+  | (bits B0 B0 B0 B0) => true
+  | (bits _ _ _ _) => false
+  end.
+
+Compute (all_zero (bits B1 B0 B1 B0)).
+(* ===> false : bool *)
+Compute (all_zero (bits B0 B0 B0 B0)).
+(* ===> true : bool *)
+
+End TuplePlayground.
+
+Module NatPlayground.
+
+Inductive nat : Type :=
+  | O
+  | S (n : nat).
+
+Inductive otherNat : Type :=
+  | stop
+  | tick (foo : otherNat).
+
+Definition pred (n : nat) : nat :=
+  match n with
+  | O => O
+  | S n' => n'
+  end.
+
+End NatPlayground.
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+  | O => O
+  | S O => O
+  | S (S n') => n'
+  end.
+
+(* Compute (pred (S(S(O)))).
+Check (S (S (S (S O)))). *)
+Compute (minustwo (S(4))).
+
+
+
